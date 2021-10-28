2debd797bf3ba186aaf1d747ecb312bfaea081ad
Nitsan Cohen <nitsancohen@Nitsans-MacBook-Pro.local>
2021-10-28 15:18:29 +0300

    token

--- sneakers/app/sneakers-store/sneakers-store.tsx
+++ sneakers/app/sneakers-store/sneakers-store.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { About } from '@nitsan770/sneakers.pages.about';
 import { Product } from '@nitsan770/sneakers.pages.product';
+import { MThemeProvider } from '@nitsan770/marketing-brand.theme.m-theme-provider';
 
 export function SneakersStore() {
   return (
     <Router>
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/">
-          <Product />
-        </Route>
-      </Switch>
+      <MThemeProvider>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Product />
+          </Route>
+        </Switch>
+      </MThemeProvider>
     </Router>
   );
 }
