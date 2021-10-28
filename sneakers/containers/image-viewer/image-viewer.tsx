debb925e22a63902a0c6df4e3b8e2ca9d3d88624
Nitsan Cohen <nitsancohen@Nitsans-MacBook-Pro.local>
2021-10-28 16:21:20 +0300

    token

--- sneakers/containers/image-viewer/image-viewer.tsx
+++ sneakers/containers/image-viewer/image-viewer.tsx
@@ -5,12 +5,14 @@ import styles from './styles.module.scss';
 export type ImageViewerProps = {};
 
 export function ImageViewer() {
+  //change
   return (
     <div className={styles.container}>
       <ProductCard
         size="big"
         imageURL="https://upload.wikimedia.org/wikipedia/commons/0/02/Air_jordan_XI_space_jam.jpg"
       />
+
       <span className={styles.viewer}>
         <ProductCard
           size="small"
